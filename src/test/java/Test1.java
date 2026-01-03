ea8948d14d30fb1daf1efa0d13d7d93ce17e15ed
Suraj-Thakkar <surajqaprofessional624@gmail.com>
2026-01-03 16:55:42 +0530

    Test to Github Action

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,32 +1,123 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.monte.media.Format;
+import org.monte.media.math.Rational;
+import org.monte.screenrecorder.ScreenRecorder;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
+
+import static org.monte.media.FormatKeys.*;
+import static org.monte.media.VideoFormatKeys.*;
 
 public class Test1 {
+
+    private ScreenRecorder screenRecorder;
+
     @Test
     public void Test() throws Exception {
+        // 1. Start Recording
+        startRecording("Yogi_Login_Test");
+
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://yogi.web.cashbook.in/login");
         driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("phoneNumber"))).sendKeys("+911000112587");
-        Thread.sleep(6000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='text']"))).sendKeys("123456");
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Verify']"))).click();
-        Thread.sleep(5000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Ok, Got it']"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='box_position_relative_xs__1dl117711e']"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='q']"))).sendKeys("Rahul Mehta");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[value='lj6wYpwHdtxTL1acFmln']"))).click();
+
+        try {
+            driver.get("https://yogi.web.cashbook.in/login");
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("phoneNumber"))).sendKeys("+911000112587");
+            Thread.sleep(6000);
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='text']"))).sendKeys("123456");
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Verify']"))).click();
+
+            Thread.sleep(5000);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Ok, Got it']"))).click();
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='box_position_relative_xs__1dl117711e']"))).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='q']"))).sendKeys("Rahul Mehta");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[value='lj6wYpwHdtxTL1acFmln']"))).click();
+
+        } catch (Exception e) {
+            // 2. Capture screenshot if any step fails
+            takeScreenshot(driver, "Test_Failure");
+            throw e;
+        } finally {
+            // 3. Stop recording and close browser
+            stopRecording();
+            driver.quit();
+        }
+    }
+
+    // --- Utility Methods ---
+
+    public void startRecording(String methodName) throws Exception {
+        File file = new File(System.getProperty("user.dir") + File.separator + "recordings");
+        if (!file.exists()) file.mkdirs();
+
+        GraphicsConfiguration gc = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice()
+                .getDefaultConfiguration();
+
+        this.screenRecorder = new SpecializedScreenRecorder(gc, gc.getBounds(),
+                new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_QUICKTIME),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_QUICKTIME_ANIMATION,
+                        CompressorNameKey, ENCODING_QUICKTIME_ANIMATION, DepthKey, 24, FrameRateKey,
+                        Rational.valueOf(15), QualityKey, 1.0f, KeyFrameIntervalKey, 15 * 60),
+                new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey, Rational.valueOf(30)),
+                null, file, methodName);
+
+        this.screenRecorder.start();
+        System.out.println("Recording started...");
+    }
+
+    public void stopRecording() throws Exception {
+        this.screenRecorder.stop();
+        System.out.println("Recording stopped.");
+    }
+
+    public void takeScreenshot(WebDriver driver, String fileName) throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File destFile = new File("./screenshots/" + fileName + "_" + timestamp + ".png");
+        FileUtils.copyFile(scrFile, destFile);
+        System.out.println("Screenshot saved at: " + destFile.getAbsolutePath());
+    }
+}
+
+// Custom Recorder Class to handle file naming
+class SpecializedScreenRecorder extends ScreenRecorder {
+    private String name;
+
+    public SpecializedScreenRecorder(GraphicsConfiguration cfg, Rectangle captureArea, Format fileFormat,
+                                     Format screenFormat, Format mouseFormat, Format audioFormat, File movieFolder, String name)
+            throws IOException, AWTException {
+        super(cfg, captureArea, fileFormat, screenFormat, mouseFormat, audioFormat, movieFolder);
+        this.name = name;
+    }
+
+    @Override
+    protected File createMovieFile(Format fileFormat) throws IOException {
+        if (!movieFolder.exists()) movieFolder.mkdirs();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        return new File(movieFolder, name + "_" + dateFormat.format(new Date()) + ".mp4");
     }
 }
